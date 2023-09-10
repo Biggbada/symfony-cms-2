2457f74e560967582c40f7a00eedf2f30d8bd18a
Biggbada <yguenot@polepo.fr>
2023-09-10 17:53:35 +0200

    comment ajax fetch is now ok

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
-    #[Route('/ajax/comments', name: 'comment')]
+    #[Route('/ajax/comments', name: 'comment_add')]
     public function add(Request $request): Response
     {
         $commentData = $request->request->all('comment');
